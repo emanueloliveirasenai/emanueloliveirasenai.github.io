df9f623b3dd07eec013357daf3fd71c3a6d7706d
emanueloliveirasenai <emanuel_oliveira151@estudante.sesisenai.org.br>
2025-08-05 13:40:39 -0300

    ultimas atualizações do site, correçoes de bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,28 @@
-const btnTopo = document.getElementById('btn-topo');
-function toggleBotaoTopo() {
-    if (window.scrollY > 100) { 
-        btnTopo.classList.add('visivel'); 
-    } else {
-        btnTopo.classList.remove('visivel');
-    }
-}
-window.addEventListener('scroll', toggleBotaoTopo);
-
-document.addEventListener('DOMContentLoaded', function() {
-    const btnHamburger = document.querySelector('.btn-hamburger');
+document.addEventListener('DOMContentLoaded', () => {
     const menuLateral = document.querySelector('.menu-lateral');
+    const btnHamburger = document.querySelector('.btn-hamburger');
+    const linksDoMenuMobile = document.querySelectorAll('.menu-lateral ul li a');
+    const btnTopo = document.getElementById('btn-topo');
 
-    btnHamburger.addEventListener('click', function() {
-        menuLateral.classList.toggle('aberto');
+    if (btnHamburger && menuLateral) {
+        btnHamburger.addEventListener('click', () => {
+            menuLateral.classList.toggle('aberto');
+        });
+    }
+
+    linksDoMenuMobile.forEach(link => {
+        link.addEventListener('click', () => {
+            menuLateral.classList.remove('aberto');
+        });
     });
+
+    function toggleBotaoTopo() {
+        if (window.scrollY > 100) {
+            btnTopo.classList.add('visivel');
+        } else {
+            btnTopo.classList.remove('visivel');
+        }
+    }
+
+    window.addEventListener('scroll', toggleBotaoTopo);
 });
