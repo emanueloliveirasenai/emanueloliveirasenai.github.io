74d6558035f93f23a36e8c1797c6ab53c4409b0d
emanueloliveirasenai <emanuel_oliveira151@estudante.sesisenai.org.br>
2025-08-04 21:38:46 -0300

    inseri o script.js e algumas alterações básicas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+const btnTopo = document.getElementById('btn-topo');
+function toggleBotaoTopo() {
+    if (window.scrollY > 100) { 
+        btnTopo.classList.add('visivel'); 
+    } else {
+        btnTopo.classList.remove('visivel');
+    }
+}
+window.addEventListener('scroll', toggleBotaoTopo);
+
 document.addEventListener('DOMContentLoaded', function() {
     const btnHamburger = document.querySelector('.btn-hamburger');
     const menuLateral = document.querySelector('.menu-lateral');
